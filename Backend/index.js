95fd60195f73c534c12d81d1d242076f38d8f55c
blitzmachine <blitzm4ch1n3@gmail.com>
2024-10-10 11:57:54 +0200

    Initial commit

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+// Import Express and initialize it.
+const express = require("express")
+const path = require("path")
+const fs = require("fs")
+const app = express()
+
+// Properties
+const host = "0.0.0.0"
+const port = 3000
+const filesLocation = path.join(__dirname, "files")
+
+
+const getDirectoryContent = (dirPath) => {
+}
+
+// API
+
+app.get("/", (_, res) => {
+    res.send(path.join(__dirname, "files"))
+})
+
+try {
+    if (!fs.existsSync(filesLocation)) {
+        fs.mkdir(filesLocation, { recursive: true}, (err) => { if (err) throw err })
+        console.warn("LocalFileSharing: No direction for file storage .. creating new one.")
+    }
+
+    app.listen(port, host, () => {
+       console.log(`LocalFileSharing: Backend-Server listening at http://${host}:${port} ...`)
+   })
+} catch (error) {
+   console.log(error)
+}
