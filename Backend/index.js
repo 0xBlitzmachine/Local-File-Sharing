d9a7fa38e4d73baf5b9aa092df76f4b799f2437e
Blitzmachine <blitzm4ch1n3@gmail.com>
2024-10-16 22:14:41 +0200

    -

--- Backend/index.js
+++ Backend/index.js
@@ -2,19 +2,18 @@ require("dotenv").config()
 
 const app = require("express")()
 const fileUtils = require("./utils/fileUtils")
-const path = require("path")
-
-const filePath = path.join(__dirname, process.env.FOLDER_FROM_PATH_TO_SERVE)
+const customConsole = require("./utils/customConsole")
 
 // API
 app.get("/", (_, res) => {
-    const test = fileUtils.getDirectoryContent(filePath)
+    const test = fileUtils.getDirectoryContent(sharedDirectoryPath)
     res.send(test)
 })
 
-fileUtils.validateExistensOfDirectory(filePath)
+
+fileUtils.validateExistensOfDirectory(process.env.FOLDERNAME)
 
 app.listen(process.env.PORT, process.env.HOSTNAME, () => {
-    console.log(`LocalFileSharing: Backend-Server listening at ${process.env.HOSTNAME}:${process.env.PORT}`)
-    console.log(filePath)
+    customConsole.warn(`Server started! Listening at http://${process.env.HOSTNAME}:${process.env.PORT}`)
+    customConsole.warn(`Name of shared folder (Uploading/Downloading): ${process.env.FOLDERNAME}/`)
 })
