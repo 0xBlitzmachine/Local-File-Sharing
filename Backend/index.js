b372a397a83787733390c61e82f49d2f18d8a14f
Blitzmachine <blitzm4ch1n3@gmail.com>
2024-10-20 21:20:12 +0200

    -

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,32 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const app = require("express")()
-const fileUtils = require("./utils/fileUtils")
-const customConsole = require("./utils/customConsole")
+const express = require("express");
+const app = express();
+
+const fileUtils = require("./utils/fileUtils");
+const customConsole = require("./utils/customConsole");
 
 // API
+
 app.get("/", (_, res) => {
-    const test = fileUtils.getDirectoryContent(sharedDirectoryPath)
-    res.send(test)
-})
+  const test = fileUtils.getDirectoryContent(
+    process.env.FULL_PATH_TO_SERVE_FROM
+  );
+  res.send(test);
+});
 
+// fileUtils.validateExistensOfDirectory();
 
-fileUtils.validateExistensOfDirectory(process.env.FOLDERNAME)
+app.listen(
+  process.env.PORT | 3000,
+  process.env.HOSTNAME | "0.0.0.0",
+  function () {
+    customConsole.warn(
+      `Server started! Listening at http://${process.env.HOSTNAME}:${process.env.PORT}`
+    );
 
-app.listen(process.env.PORT, process.env.HOSTNAME, () => {
-    customConsole.warn(`Server started! Listening at http://${process.env.HOSTNAME}:${process.env.PORT}`)
-    customConsole.warn(`Name of shared folder (Uploading/Downloading): ${process.env.FOLDERNAME}/`)
-})
+    customConsole.warn(
+      `Name of shared folder (Uploading/Downloading): ${process.env.FOLDERNAME}/`
+    );
+  }
+);
--- Backend/utils/fileUtils.js
+++ Backend/utils/fileUtils.js
@@ -1,30 +1,48 @@
-const customConsole = require("./customConsole")
-const fs = require("fs")
-const path = require("path")
+const customConsole = require("./customConsole");
+const fs = require("fs");
+const path = require("path");
 
 class FileUtils {
-    static getDirectoryContent(dirPath) {
-        const content = fs.readdirSync(dirPath).map((file) => {
-            const fullPath = path.join(dirPath, file)
-    
-            return fs.statSync(fullPath).isDirectory()
-            ? { name: file, type: "directory", content: this.getDirectoryContent(fullPath) } 
-            : { name: file, type: "file"}
-        })
-
-        return content
+  static #FILES_PATH = () => {
+    switch (process.env.SERVE_FROM_ROOT) {
+      case "true":
+        return path.join(__dirname, "..", process.env.FOLDERNAME);
+      default:
+        return process.env.FULL_PATH_TO_SERVE_FROM;
     }
+  };
 
-    static validateExistensOfDirectory(dirPath) {
-        try {
-            if (!fs.existsSync(dirPath)) {
-                fs.mkdir(dirPath, { recursive: true }, (err) => { if (err) throw err })
-                customConsole.warn("Set directory not found! Creating new one.")
-            }
-        } catch (err) {
-            customConsole.error(err)
-        }
+  static getDirectoryContent(dir = FileUtils.#FILES_PATH) {
+    const content = fs.readdirSync(dir).map((file) => {
+      const fullPath = path.join(dir, file);
+
+      // Converted to MB
+      const size = fs.statSync(fullPath).size / 1048578;
+
+      return fs.statSync(fullPath).isDirectory()
+        ? {
+            name: file,
+            type: "directory",
+            content: this.getDirectoryContent(fullPath),
+          }
+        : { name: file, type: "file", size: size };
+    });
+
+    return content;
+  }
+
+  static validateExistensOfDirectory() {
+    try {
+      if (!fs.existsSync(FileUtils.#FILES_PATH)) {
+        fs.mkdir(FileUtils.#FILES_PATH, { recursive: true }, (err) => {
+          if (err) throw err;
+        });
+        customConsole.warn("Set directory not found! Creating new one.");
+      }
+    } catch (err) {
+      customConsole.error(err);
     }
+  }
 }
 
-module.exports = FileUtils
+module.exports = FileUtils;
